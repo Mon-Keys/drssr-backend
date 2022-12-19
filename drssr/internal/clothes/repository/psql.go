a672980bfb54957b3c4e46f813e588eec7d17d83
Ilyagu <inagdimaev@mail.ru>
2022-12-19 20:34:34 +0200

    add translate

--- drssr/internal/clothes/repository/psql.go
+++ drssr/internal/clothes/repository/psql.go
@@ -70,7 +70,7 @@ func (pr *postgresqlRepository) AddClothes(ctx context.Context, clothes models.C
 			mask,
 			owner_id,
 			created_at;`,
-		clothes.Type,
+		models.ToEnglishType[clothes.Type],
 		clothes.ImgPath,
 		clothes.MaskPath,
 		clothes.OwnerID,
@@ -84,6 +84,8 @@ func (pr *postgresqlRepository) AddClothes(ctx context.Context, clothes models.C
 		&createdClothes.OwnerID,
 		&createdClothes.Ctime,
 	)
+
+	createdClothes.Type = models.ToRussianType[createdClothes.Type]
 
 	if err != nil {
 		return models.Clothes{}, err
@@ -113,7 +115,7 @@ func (pr *postgresqlRepository) UpdateClothes(ctx context.Context, newClothesDat
 			owner_id,
 			created_at;`,
 		newClothesData.ID,
-		newClothesData.Type,
+		models.ToEnglishType[newClothesData.Type],
 		newClothesData.Name,
 		newClothesData.Desc,
 		newClothesData.Color,
@@ -138,6 +140,8 @@ func (pr *postgresqlRepository) UpdateClothes(ctx context.Context, newClothesDat
 		&updatedClothes.OwnerID,
 		&updatedClothes.Ctime,
 	)
+
+	updatedClothes.Type = models.ToRussianType[updatedClothes.Type]
 
 	if err != nil {
 		return models.Clothes{}, err
@@ -170,7 +174,11 @@ func (pr *postgresqlRepository) GetClothesMaskByTypeAndSex(
 	clothesType string,
 	clothesSex string,
 ) ([]models.Clothes, error) {
-	rows, err := pr.conn.Query(`SELECT id, mask FROM clothes WHERE type = $1 AND sex = $2;`, clothesType, clothesSex)
+	rows, err := pr.conn.Query(
+		`SELECT id, mask FROM clothes WHERE type = $1 AND sex = $2;`,
+		models.ToEnglishType[clothesType],
+		clothesSex,
+	)
 	if err != nil {
 		return []models.Clothes{}, err
 	}
@@ -311,6 +319,7 @@ func (pr *postgresqlRepository) GetAllClothes(ctx context.Context, limit, offset
 		if err != nil {
 			return []models.Clothes{}, err
 		}
+		row.Type = models.ToRussianType[row.Type]
 		respList = append(respList, row)
 	}
 	if err := rows.Err(); err != nil {
@@ -373,6 +382,7 @@ func (pr *postgresqlRepository) GetUsersClothes(ctx context.Context, limit, offs
 		if err != nil {
 			return []models.Clothes{}, err
 		}
+		row.Type = models.ToRussianType[row.Type]
 		respList = append(respList, row)
 	}
 	if err := rows.Err(); err != nil {
@@ -422,6 +432,8 @@ func (pr *postgresqlRepository) GetClothesByID(ctx context.Context, cid uint64) 
 	if err != nil {
 		return models.Clothes{}, err
 	}
+
+	clothes.Type = models.ToRussianType[clothes.Type]
 
 	return clothes, nil
 }
